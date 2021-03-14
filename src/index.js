3ee58540434eaad96a67332600f9f890997b30bb
nikitar69 <nikitar69@gmail.com>
2021-03-14 18:31:29 +0100

    feat: compele task func

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  let sorted = []
+
+  if (matrix == undefined) {
+    return sorted
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    let row = matrix[i];
+    if (i % 2 === 0) {
+      for (let j = 0; j < row.length; j++) {
+        sorted.push(row[j])
+      }
+    } else {
+      row.reverse()
+      for (let j = 0; j < row.length; j++) {
+        sorted.push(row[j])
+      }
+    }
+
+
+  }
+  return sorted
+
 }
